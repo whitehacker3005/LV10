2b1119f57771fc0253ddfbd597898b628a96c6ac
„Ivan <ivan.jelic.frc@gmail.com>
2024-01-15 15:14:26 +0100

    dodan blinky program

--- USR_LV10/Core/Src/main.c
+++ USR_LV10/Core/Src/main.c
@@ -94,7 +94,10 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-
+	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+	  HAL_Delay(200);
+	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+	  HAL_Delay(200);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
